3074a4611885c5ab02fb80f5d0dcf6a69dfea6a7
dathoangquoc <dathgqc@gmail.com>
2025-05-15 17:43:15 +0700

    bug due to model selection

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import asyncio
 import json
 from datetime import datetime, timezone
-from logging import INFO
+import logging 
 
 from graphiti_core import Graphiti
 from graphiti_core.nodes import EpisodeType
 from graphiti_core.search.search_config_recipes import NODE_HYBRID_SEARCH_RRF
 
+from graphiti_core.llm_client.gemini_client import GeminiClient
+from graphiti_core.embedder.gemini import GeminiEmbedder, GeminiEmbedderConfig
+
 from graphiti_core.llm_client.openai_generic_client import OpenAIGenericClient
 from graphiti_core.llm_client import LLMConfig
 
+from pydantic import BaseModel, Field
+
 from sentence_transformer_patch import SentenceTransformerEmbedder, SentenceTransformerCrossEncoder
+
+logging.basicConfig(filename='debug.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # neo4j configs
 NEO4J_URI = "bolt://localhost:7687"
@@ -20,7 +27,7 @@ NEO4J_PASSWORD = "password"
 # LLM configs
 LLM_API_KEY = "dummy"
 LLM_BASE_URL = "http://localhost:11434/v1"
-LLM_MODEL = "llama3.2:1b"
+LLM_MODEL = "llama3.2:3b"
 
 # Embedder configs
 EMBEDDER_API_KEY = "dummy"
@@ -30,6 +37,8 @@ EMBEDDER_MODEL = "nomic-embed-text"
 SENTENCE_TRANSFORMER_MODEL = "all-MiniLM-L6-v2"
 EMBEDDING_DIM = 384
 
+logger = logging.getLogger(__name__)
+logger.critical(f"Current model {LLM_MODEL}: ") 
 
 llm_config = LLMConfig(
     model=LLM_MODEL,
@@ -43,14 +52,36 @@ embedder_config = LLMConfig(
     base_url=EMBEDDER_BASE_URL,
 )
 
+gemini = GeminiClient(
+    config=LLMConfig(
+        api_key='dunnofam',
+        model="gemini-2.0-flash"
+    )
+)
+
+gemini_emb = GeminiEmbedder(
+    config=GeminiEmbedderConfig(
+        api_key='dunnofam',
+        embedding_model='embedding-001'
+    )
+)
+# # Entity Types
+# class Person(BaseModel):
+#     role: str | None = Field(..., description="The role of the person")
+
+# class Place(BaseModel):
+#     location: str | None = Field(..., description="The location of the place")
+
+# entity_types = {"Person": Person, "Place": Place}
+
 async def main():
     # Initialize Graphiti with Neo4j connection
     graphiti = Graphiti(
         uri=NEO4J_URI,
         user=NEO4J_USER,
         password=NEO4J_PASSWORD,
-        llm_client=OpenAIGenericClient(llm_config),
-        embedder=SentenceTransformerEmbedder(SENTENCE_TRANSFORMER_MODEL, EMBEDDING_DIM),
+        llm_client=gemini,
+        embedder=gemini_emb,
         cross_encoder=SentenceTransformerCrossEncoder(SENTENCE_TRANSFORMER_MODEL)
     )
 
@@ -105,6 +136,7 @@ async def main():
                 source=episode['type'],
                 source_description=episode['description'],
                 reference_time=datetime.now(timezone.utc),
+                # entity_types=entity_types
             )
             print(f'Added episode: Freakonomics Radio {i} ({episode["type"].value})')
 
--- test_ollama.sh
+++ test_ollama.sh
@@ -1,15 +1,15 @@
 curl http://localhost:11434/v1/chat/completions \
-    -H "Content-Type: application/json" \
-    -d '{
-        "model": "llama3.2:1b",
-        "messages": [
-            {
-                "role": "system",
-                "content": "You are a helpful assistant."
-            },
-            {
-                "role": "user",
-                "content": "Hello!"
-            }
-        ]
-    }'
+  -H "Content-Type: application/json" \
+  -d '{
+    "model": "llama3.2:3b",
+    "messages": [
+      {
+        "role": "system",
+        "content": "You are an AI assistant that extracts entity nodes from text. \nYour primary task is to extract and classify the speaker and other significant entities mentioned in the provided text.\nDo not escape unicode characters.\nAny extracted information should be returned in the same language as it was written in."
+      },
+      {
+        "role": "user",
+        "content": "\n<TEXT>\nKamala Harris is the Attorney General of California. She was previously the district attorney for San Francisco.\n</TEXT>\n<ENTITY TYPES>\n[{\"entity_type_id\": 0, \"entity_type_name\": \"Entity\", \"entity_type_description\": \"Default entity classification. Use this entity type if the entity is not one of the other listed types.\"}, {\"entity_type_id\": 1, \"entity_type_name\": \"Person\", \"entity_type_description\": null}, {\"entity_type_id\": 2, \"entity_type_name\": \"Place\", \"entity_type_description\": null}]\n</ENTITY TYPES>\n\nGiven the above text, extract entities from the TEXT that are explicitly or implicitly mentioned.\nFor each entity extracted, also determine its entity type based on the provided ENTITY TYPES and their descriptions.\nIndicate the classified entity type by providing its entity_type_id.\n\nGuidelines:\n1. Extract significant entities, concepts, or actors mentioned in the conversation.\n2. Avoid creating nodes for relationships or actions.\n3. Avoid creating nodes for temporal information like dates, times or years (these will be added to edges later).\n4. Be as explicit as possible in your node names, using full names and avoiding abbreviations.\n\nRespond with a JSON object in the following format:\n\n{\"$defs\": {\"ExtractedEntity\": {\"properties\": {\"name\": {\"description\": \"Name of the extracted entity\", \"title\": \"Name\", \"type\": \"string\"}, \"entity_type_id\": {\"description\": \"ID of the classified entity type. Must be one of the provided entity_type_id integers.\", \"title\": \"Entity Type Id\", \"type\": \"integer\"}}, \"required\": [\"name\", \"entity_type_id\"], \"title\": \"ExtractedEntity\", \"type\": \"object\"}}, \"properties\": {\"extracted_entities\": {\"description\": \"List of extracted entities\", \"items\": {\"$ref\": \"#/$defs/ExtractedEntity\"}, \"title\": \"Extracted Entities\", \"type\": \"array\"}}, \"required\": [\"extracted_entities\"], \"title\": \"ExtractedEntities\", \"type\": \"object\"}"
+      }
+    ]
+  }'
